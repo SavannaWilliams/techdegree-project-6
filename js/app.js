d2427279ea2fdeabebc525896547eb37f9dcbff3
SavannaWilliams <54858237+SavannaWilliams@users.noreply.github.com>
2020-08-12 13:17:57 -0400

    Begin work on reset button, reset missed, reset letter keys.

--- js/app.js
+++ js/app.js
@@ -1,18 +1,24 @@
 // Get the letter input keys
-let keyboard= document.getElementById('qwerty');
+let keyboard = document.getElementById('qwerty');
+let keyBtns = document.querySelectorAll('.keyrow button');
 // Get the empty 'phrase' div
 let phrase = document.getElementById('phrase');
 // Set the starting incorrect letters to 0
 let missed = 0;
-// Get the start button and intro overlay
+// Get the start button, intro overlay, and overlay header text.
 let startBtn = document.getElementsByClassName('btn__reset')[0];
 let overlay = document.getElementById('overlay');
+let overlayTitle = document.querySelector('#overlay .title');
 // Get all letters on the phrase board
 let letters = document.getElementsByClassName('letter');
 // Get all shown letters on the phrase board
 let shown = document.getElementsByClassName('show');
-
-
+// Prepare for reset button
+let resetBtn = document.createElement('a');
+resetBtn.textContent = "Play again?";
+resetBtn.classList.add('btn__reset');
+overlay.appendChild(resetBtn);
+resetBtn.style.display = "none";
 // Hide the intro overlay when start button clicked
 startBtn.addEventListener('click', function() {
     overlay.style.display = 'none';
@@ -57,46 +63,84 @@ function addPhraseToDisplay(arr){
 }
 
 function checkLetter (btn){
-    // Get all the list elements with a class of 'letter'.
-    let match = '';
-    for (let i = 0; i < letters.length; i++) {
-        // Get the current list item.
-        let currentLi = letters[i];
-        // Get the text content of the current list item.
-        let currentLtr = letters[i].textContent;
+    // Checking for undefined stops it for adding to Missed when clicking the keyboard's white space.
+    if (btn !== undefined) {
+        // Get all the list elements with a class of 'letter'.
+        let match = '';
+        for (let i = 0; i < letters.length; i++) {
+            // Get the current list item.
+            let currentLi = letters[i];
+            // Get the text content of the current list item.
+            let currentLtr = letters[i].textContent;
 
-        // If the current letter matches the pushed button, give it the class 'show' to show the tile.
-        if (currentLtr === btn) {
-            currentLi.classList.add('show');
-            match = currentLtr;
+            // If the current letter matches the pushed button, give it the class 'show' to show the tile.
+            if (currentLtr === btn) {
+                currentLi.classList.add('show');
+                match = currentLtr;
+            }
         }
-    }
 
-    if (match !== '') {
-        return match;
-    } else {
-        return null;
+        if (match !== '') {
+            return match;
+        } else {
+            return null;
+        }
     }
 }
 
 function checkWin () {
     let showLength = shown.length;
     let ltrsLength = letters.length;
+    // Win- If shown letters are equal to the total number of letters in the phrase, the player wins.
     if (showLength === ltrsLength) {
-        overlay.style.display = 'initial';
-        console.log('Win');
+        // Update the overlay
+        overlay.style.display = '';
+        overlay.classList.remove('start');
+        overlay.classList.add('win');
+        overlayTitle.textContent = "You won!";
+        // Create the reset button
+        startBtn.remove();
+        resetBtn.style.display = "";
 
+        resetBtn.addEventListener('click', (e) => {
+            missed = 0;
+            for (let i = 0; i < letters.length; i++) {
+                // Get the current list item.
+                let currentLi = letters[i];
+                currentLi.classList.remove('show');
+            }
+            keyBtns.forEach(btn => {
+                btn.classList.remove('chosen');
+                btn.disabled = false;
+            });
+
+            overlay.style.display = 'none';
+        });
+        
+    // Lose- If the play has had 5 wrong choices, they lose.
     } else if (missed === 5) {
-        overlay.style.display = 'initial';
-        console.log('Lose');
+        // Update the overlay
+        overlay.style.display = '';
+        overlay.classList.remove('start');
+        overlay.classList.add('lose');
+        overlayTitle.textContent = "You lost.";
+        // Create the reset button
+        startBtn.remove();
+        resetBtn.style.display = "";
     }
 }
 
 keyboard.addEventListener('click', (e) => {
+    let btn;
+    let clicked = e.target;
+
+    // Check that the clicked element is a letter button.
+    if (clicked.tagName == 'BUTTON') {
     // Add the class 'chosen' to the clicked letter and disable the key so it can't be clicked again.
-    e.target.classList.add('chosen');
-    e.target.disabled = true;
-    let btn = e.target.textContent;
+        clicked.classList.add('chosen');
+        clicked.disabled = true;
+        btn = clicked.textContent;
+    }
 
     // Run checkLetter and store result in letterFound.
     let letterFound = checkLetter(btn);
