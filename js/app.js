c8de82e3db494f5bfac4cc8a95c31aac22126728
SavannaWilliams <54858237+SavannaWilliams@users.noreply.github.com>
2020-08-15 12:53:09 -0400

    Add transitions and animations and begin work on win phrase.

--- js/app.js
+++ js/app.js
@@ -3,12 +3,16 @@ let keyboard = document.getElementById('qwerty');
 let keyBtns = document.querySelectorAll('.keyrow button');
 // Get the empty 'phrase' div
 let phrase = document.getElementById('phrase');
+// Prepare for the phrase choice.
+let currentPhrase;
 // Set the starting incorrect letters to 0
 let missed = 0;
 // Get the start button, intro overlay, and overlay header text.
 let startBtn = document.querySelector('.btn__reset');
 let overlay = document.getElementById('overlay');
 let overlayTitle = document.querySelector('#overlay .title');
+// Preparing for the dynamically added win phrase.
+let winPhrase;
 // Get the document's phrase ul.
 let ul = document.querySelector('#phrase ul');
 // Get all letters on the phrase board
@@ -22,33 +26,49 @@ let lastHeart = -1;
 
 // Start and reset button event listener
 startBtn.addEventListener('click', function(e) {
-    // Hide overlay and remove start class to prepare for win or loss.
-    overlay.style.display = 'none';
-    overlay.classList.remove('start');
+    // Remove chosen keyboard styling.
+    keyBtns.forEach(btn => {
+    btn.classList.remove('chosen');
+    btn.disabled = false;
+    });
 
     // Remove shown letters.
     for (let i = 0; i < letters.length; i++) {
         let currentLi = letters[i];
         currentLi.classList.remove('show');
     }
-    // Remove chosen keyboard styling.
-    keyBtns.forEach(btn => {
-        btn.classList.remove('chosen');
-        btn.disabled = false;
-    });
-    // Change start button into reset button by changing button text.
-    startBtn.textContent = "Play again?";
 
-    // Check for and remove overlay classes to prepare for new win or loss.
-    if (overlay.classList.contains('win')){
-        overlay.classList.remove('win');
-    } else if (overlay.classList.contains('lose')){
-        overlay.classList.remove('lose');
+    // Remove none display type on hearts and check for and remove hidden class.
+    for (let i = 0; i < hearts.length; i++) {
+        let currentHeart = hearts[i];
+        currentHeart.style.display = '';
+        if (currentHeart.classList.contains('hidden')){
+            currentHeart.classList.remove('hidden');
+        }
     }
+
+    // Overlay styles on delay to ensure board is cleared before revealed.
+    setTimeout(function(){
+        // Hide overlay and remove start class to prepare for win or loss.
+        overlay.style.display = 'none';
+        if (overlay.classList.contains('start')){
+            overlay.classList.remove('start');
+        }
+
+        // Change start button into reset button by changing button text.
+        startBtn.textContent = "Play again?";
+
+        // Check for and remove overlay classes to prepare for new win or loss.
+        if (overlay.classList.contains('win')){
+            overlay.classList.remove('win');
+        } else if (overlay.classList.contains('lose')){
+            overlay.classList.remove('lose');
+        }
+    }, 200);
 });
 
 // Array of phrases for the game
-const phrases = ['this is a test phrase', 'here is another test phrase', 'yet another phrase to test'];
+const phrases = ['javascript is fun', 'bootstrap is a framework', 'react was developed by facebook', 'visual studio code is free', 'html is a markup language'];
 
 function getRandomPhraseAsArray (arr) {
     // Get array's length, then + 1 to account for the fact that maxLength's value is exclusive in randNum.
@@ -57,10 +77,10 @@ function getRandomPhraseAsArray (arr) {
     // This is why maxLength needs to be one more than the true length, because otherwise randNum would never pick the last string.
     let randNum = Math.floor(Math.random() * (maxLength - 1)) + 1;
     // Arrays start at zero, so to include that first string as a possible choice, subtract one from randNum.
-    let choice = arr[randNum - 1];
-    console.log(choice);
+    currentPhrase = arr[randNum - 1];
+    console.log(currentPhrase);
     // Split the string up by character.
-    return choice.split('');
+    return currentPhrase.split('');
 }
 
 function addPhraseToDisplay(arr){
@@ -128,21 +148,28 @@ function checkWin () {
     let ltrsLength = letters.length;
     // Win: If shown letters are equal to the total number of letters in the phrase, the player wins.
     if (showLength === ltrsLength) {
-        // Update the overlay
-        overlay.style.display = '';
-        overlay.classList.add('win');
-        overlayTitle.textContent = "You won!";
-        //Reset missed.
-        resetGame();
+        // Update the overlay after .28 seconds (enough time for the final heart to disappear).
+        setTimeout(function(){
+            overlay.style.display = '';
+            overlay.classList.add('win');
+            winPhrase = overlay.prepend(`Correct! "${currentPhrase}."`);
+            winPhrase.classList.add('win-phrase');
+            overlayTitle.textContent = "You won!";
+            //Reset missed.
+            resetGame();
+        }, 280);
+
     // Lose: If the play has had 5 wrong choices, they lose.
     } else if (missed === 5) {
-        // Update the overlay
-        overlay.style.display = '';
-        overlay.classList.remove('start');
-        overlay.classList.add('lose');
-        overlayTitle.textContent = "You lost.";
-        //Reset game.
-        resetGame();
+        // Update the overlay after .28 seconds.
+        setTimeout(function(){
+            overlay.style.display = '';
+            overlay.classList.remove('start');
+            overlay.classList.add('lose');
+            overlayTitle.textContent = "You lost.";
+            //Reset game.
+            resetGame();
+        }, 280);
     }
 }
 
@@ -165,7 +192,17 @@ keyboard.addEventListener('click', (e) => {
     if (letterFound === null) {
         missed += 1;
         lastHeart +=1;
-        hearts[lastHeart].style.display = 'none';
+        // If heart is not the fifth heart, add .hidden which contains transitions and an animation.
+        if (lastHeart !== 4) {
+            hearts[lastHeart].classList.add('hidden');
+            // Delay setting the display to none until the animations have finished (in 1 second).
+            setTimeout(function(){
+                hearts[lastHeart].style.display = 'none';
+            }, 1000);
+        // If the heart is the fifth heart, skip the animations and hide immediately.
+        } else {
+            hearts[lastHeart].style.display = 'none';
+        }
     }
 
     // Check for win lose condition.
